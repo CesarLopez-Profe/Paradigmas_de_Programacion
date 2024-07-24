a79e6ba8f600b969cd930f2f327a4b5fef6b42b1
calopezg <calopezg@gmail.com>
2024-07-24 11:30:17 -0500

    Primer Commit Material Curso

---
+++ Proyectos/p_bicicleta/p_bicicleta/Clases/Bicicleta.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace p_bicicleta.Clases
+{
+    internal class Bicicleta
+    {
+        public enum l_tamanos { S, M, L };
+        public enum l_colores {Cromo, Negro, Rojo };
+        public enum l_materiales {Alumnio, Fibra, Carbono };
+        
+        //Atributos de Propiedad
+        public string marca;
+        public l_tamanos tam_marco;
+        public float tam_llanta;
+        public byte nro_cambios;
+        public l_colores color;
+        public l_materiales material;
+
+        //Atributos de Estado
+        public byte cambio_actual = 1;
+        public float vel_actual = 0;
+
+
+        public byte Subir_cambio()
+        {
+            try
+            {
+                if (cambio_actual < nro_cambios) 
+                    cambio_actual++;
+
+                return cambio_actual;
+
+            }
+            catch(Exception error)
+            {
+                throw new Exception("Ocurrió un error subiendo el cambio \n" + error);
+            }
+
+
+
+        }
+
+
+
+    }
+}
---
+++ README.md
@@ -0,0 +1,26 @@
+# Paradigmas
+
+Material de estudio para Paradigmas de programación.
+
+En este directorio encontrarán todo el material desarollado para el apoyo en clase. 	
+
+Cabe aclarar que esta tabla resume brevemente el contenido de cada documento, para ver la información completa deberá descargar el archivo.
+
+## Documentos	
+
+  • [Trayecto de la materia](Planeacion_curso_paradigmas_2024-2.xlsx) 
+
+  • [Los Mandamientos del programador](https://www.canva.com/design/DAGLDNHOZ0A/tjMNPkM1jW5kIDijSATwRA/edit?utm_content=DAGLDNHOZ0A&utm_campaign=designshare&utm_medium=link2&utm_source=sharebutton)
+
+
+## Paradigma Orientado a Objetos
+  
+  • [Conceptos Básicos de Clases]
+
+  • [Accesabilidad, Constructores, Accesores]
+
+  • [Relaciones entre clases]
+
+  • [Herencia]
+
+  • [Polimorfismo]
